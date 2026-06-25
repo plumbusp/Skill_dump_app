228395e3a610d8161889084b6c84b6abc99bcf6f
minimus <129208104+plumbusp@users.noreply.github.com>
2026-06-25 18:00:59 +0300

    Fixed issue with profile picture not showing in deployment

--- app.py
+++ app.py
@@ -61,7 +61,12 @@ def show_home():
     if "user_id" in session:
         total_skills = users.get_total_skills(session["user_id"])
         skill_stats = users.get_skill_stats(session["user_id"])
-        return render_template("index.html", total_skills=total_skills, skill_stats=skill_stats)
+        user_image = users.get_image(session["user_id"])
+        if not user_image:
+            return render_template("index.html", total_skills=total_skills, skill_stats=skill_stats)
+        else:
+            return render_template("index.html", total_skills=total_skills, skill_stats=skill_stats,user_image =user_image)
+    
     else:
         filled = {}
         username = request.args.get("username")
